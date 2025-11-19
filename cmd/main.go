719d015d326b6e728cd01a8e667419b87932a74f
Nelo-T. Wallus <n.wallus@sap.com>
2025-11-19 13:37:32 +0100

    Add APIs
    kubebuilder create api --group example --version v1alpha1 --kind Source
    kubebuilder create api --group example --version v1alpha1 --kind Target
    
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

--- cmd/main.go
+++ cmd/main.go
@@ -34,6 +34,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	examplev1alpha1 "github.com/ntnn/mcr-scheme-headaches/api/v1alpha1"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -45,6 +47,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(examplev1alpha1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
